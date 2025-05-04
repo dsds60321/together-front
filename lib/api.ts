6150dabdcbd046acfd8ddf62f784f5809ce1f9ab
dsds60321 <dsds60321@gmail.com>
2025-05-04 23:54:58 +0900

    fix : api

--- lib/api.ts
+++ lib/api.ts
@@ -4,7 +4,7 @@ import { Place } from './data';
 
 // axios 인스턴스 생성
 const apiClient = axios.create({
-    baseURL: 'https://https://gunho.dev',
+    baseURL: 'https://gunho.dev/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
